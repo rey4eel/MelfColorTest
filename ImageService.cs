40cf541269f576336692959e1e2290c59710d4c4
rey4eel <sakort39@gmail.com>
2020-11-01 17:11:13 +0100

    testing separate parts

---
+++ ImageService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+
+
+namespace MelfColorTest
+{
+    class ImageService
+    {
+
+        public Image GetImage()
+        {
+            var s = File.ReadAllText(@"C:\Users\vardan.saakian\source\repos\ImageConverter\TextFile1.txt");
+            if (s.StartsWith("0x"))
+            s = s.Remove(0, 2);
+            using (var stream = new MemoryStream(ConvertHexStringToByteArray(s)))
+            {
+                var image = new Bitmap(stream);
+                return image;
+            }
+        }
+
+        public static byte[] ConvertHexStringToByteArray(string hexString)
+        {
+            if (hexString.Length % 2 != 0)
+            {
+                throw new ArgumentException(String.Format(CultureInfo.InvariantCulture, "The binary key cannot have an odd number of digits: {0}", hexString));
+            }
+
+            byte[] HexAsBytes = new byte[hexString.Length / 2];
+            for (int index = 0; index < HexAsBytes.Length; index++)
+            {
+                string byteValue = hexString.Substring(index * 2, 2);
+                HexAsBytes[index] = byte.Parse(byteValue, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+            }
+
+            return HexAsBytes;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace MelfColorTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void control1_LostFocus(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void control1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Debug.Print(this.control1.TransformToAncestor(Application.Current.MainWindow).Transform(new Point(0, 0)).ToString());
+        }
+    }
+}
